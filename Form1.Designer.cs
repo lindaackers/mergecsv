0590ff55e6d5556bea5bdd5223242c6a17e159ed
lindaackers <linda.ackers@gmail.com>
2018-01-31 11:27:05 -0800

    working draft

---
+++ Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace MergeFiles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxFile1 = new System.Windows.Forms.TextBox();
+            this.textBoxFile2 = new System.Windows.Forms.TextBox();
+            this.buttonFile1 = new System.Windows.Forms.Button();
+            this.buttonFile2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.buttonProcess = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxFile1
+            // 
+            this.textBoxFile1.Location = new System.Drawing.Point(48, 43);
+            this.textBoxFile1.Multiline = true;
+            this.textBoxFile1.Name = "textBoxFile1";
+            this.textBoxFile1.ReadOnly = true;
+            this.textBoxFile1.Size = new System.Drawing.Size(393, 56);
+            this.textBoxFile1.TabIndex = 0;
+            this.textBoxFile1.Text = "Select File";
+            // 
+            // textBoxFile2
+            // 
+            this.textBoxFile2.Location = new System.Drawing.Point(48, 105);
+            this.textBoxFile2.Multiline = true;
+            this.textBoxFile2.Name = "textBoxFile2";
+            this.textBoxFile2.ReadOnly = true;
+            this.textBoxFile2.Size = new System.Drawing.Size(393, 56);
+            this.textBoxFile2.TabIndex = 1;
+            this.textBoxFile2.Text = "Select File";
+            // 
+            // buttonFile1
+            // 
+            this.buttonFile1.Location = new System.Drawing.Point(447, 59);
+            this.buttonFile1.Name = "buttonFile1";
+            this.buttonFile1.Size = new System.Drawing.Size(90, 19);
+            this.buttonFile1.TabIndex = 2;
+            this.buttonFile1.Text = "Select File 1";
+            this.buttonFile1.UseVisualStyleBackColor = true;
+            this.buttonFile1.Click += new System.EventHandler(this.buttonFile1_Click);
+            // 
+            // buttonFile2
+            // 
+            this.buttonFile2.Enabled = false;
+            this.buttonFile2.Location = new System.Drawing.Point(447, 124);
+            this.buttonFile2.Name = "buttonFile2";
+            this.buttonFile2.Size = new System.Drawing.Size(90, 19);
+            this.buttonFile2.TabIndex = 3;
+            this.buttonFile2.Text = "Select File 2";
+            this.buttonFile2.UseVisualStyleBackColor = true;
+            this.buttonFile2.Click += new System.EventHandler(this.buttonFile2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // buttonProcess
+            // 
+            this.buttonProcess.Location = new System.Drawing.Point(57, 239);
+            this.buttonProcess.Name = "buttonProcess";
+            this.buttonProcess.Size = new System.Drawing.Size(75, 23);
+            this.buttonProcess.TabIndex = 4;
+            this.buttonProcess.Text = "Process data";
+            this.buttonProcess.UseVisualStyleBackColor = true;
+            this.buttonProcess.Click += new System.EventHandler(this.buttonProcess_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(730, 364);
+            this.Controls.Add(this.buttonProcess);
+            this.Controls.Add(this.buttonFile2);
+            this.Controls.Add(this.buttonFile1);
+            this.Controls.Add(this.textBoxFile2);
+            this.Controls.Add(this.textBoxFile1);
+            this.Name = "Form1";
+            this.Text = "Compare Files";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxFile1;
+        private System.Windows.Forms.TextBox textBoxFile2;
+        private System.Windows.Forms.Button buttonFile1;
+        private System.Windows.Forms.Button buttonFile2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button buttonProcess;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic.FileIO;
+
+namespace MergeFiles
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string fileName1 = null;
+        string fileName2 = null;
+
+        public string BrowseFile()
+        {
+            openFileDialog1.Filter = "csv files (*.csv)|*.csv";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.RestoreDirectory = true;
+            openFileDialog1.FileName = "";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string path = openFileDialog1.FileName;
+                string filename = System.IO.Path.GetFileName(path);
+                //MessageBox.Show(path + filename); //debug
+                return path;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        /*http://codeskaters.blogspot.ae/2015/11/c-easiest-csv-parser-built-in-net.html*/
+        private void ParseFile(string filename)
+        {
+            using (TextFieldParser csvParser = new TextFieldParser(filename))
+            {
+                csvParser.CommentTokens = new string[] { "#" };
+                csvParser.SetDelimiters(new string[] { "," });
+                csvParser.HasFieldsEnclosedInQuotes = true;
+
+                // Skip the row with the column names
+                //csvParser.ReadLine(); //Use this to get headings  
+
+                while (!csvParser.EndOfData)
+                {
+                    // Read current line fields, pointer moves to the next line.
+                    string[] fields = csvParser.ReadFields();
+                    int arrayLength = fields.GetLength(0);
+                    //MessageBox.Show(arrayLength.ToString());
+                    string MMSID = fields[0];
+                    string Title = fields[1];
+                    MessageBox.Show(Title, "Titles");
+                }
+            }
+        }
+
+       
+
+        private void buttonFile1_Click(object sender, EventArgs e)
+        {
+            fileName1 = BrowseFile();
+            if (fileName1==null)
+            {
+                MessageBox.Show("No file choosen. Please try again.", "Warning");
+            }
+            else
+            {
+                MessageBox.Show(fileName1);
+                textBoxFile1.Text = fileName1;
+                buttonFile2.Enabled = true;
+            }
+            
+        }
+
+        private void buttonFile2_Click(object sender, EventArgs e)
+        {
+            fileName2 = BrowseFile();
+            if (fileName2 == null)
+            {
+                MessageBox.Show("No file choosen. Please try again.", "Warning");
+            }
+            else
+            {
+                MessageBox.Show(fileName2);
+                textBoxFile2.Text = fileName2;
+            }
+        }
+
+        private void buttonProcess_Click(object sender, EventArgs e)
+        {
+            ParseFile(fileName1);
+        }
+    }
+}
